5fe6ce3d1a7d5bccf6b87c24226ca6d7716b9d3a
Charles Butler <cdbutle@outlook.com>
2021-02-12 03:51:27 +0000

    Automatically backed up by Learn

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,7 @@
-module Players
-  class Human < Player
-    def move(board)
-      user_position = gets.chomp
-      gets.chomp
-    end
+class Player
+  attr_reader :token
+
+  def initialize(token)
+    @token = token
   end
 end
